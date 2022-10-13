5d4400522f6a4378fa174a058baec1ba13c354de
geoffroy2 <geoffroyfleurylebig@gmail.com>
2022-10-13 16:22:40 +0000

    dd

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -90,6 +90,16 @@ export class Product extends TimesTampEntities {
   @Column({ nullable: true })
   category_id: string;
 
+  @ManyToOne(() => Categorie, (categorie) => categorie.products, {
+    cascade: true,
+    nullable: true,
+  })
+  @JoinColumn({
+    name: 'category_id',
+    referencedColumnName: 'id',
+  })
+  categorie: Categorie;
+
   @ManyToMany(() => Color, {
     eager: true,
   })
